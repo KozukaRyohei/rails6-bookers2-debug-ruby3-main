5ced22033d09aa477aa70189923489c9b0233756
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-09-10 06:56:23 +0000

    [Finish] 応用課題7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,11 @@ class UsersController < ApplicationController
     end
     @books = @user.books
     @book = Book.new
+    #投稿数表示関連
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
